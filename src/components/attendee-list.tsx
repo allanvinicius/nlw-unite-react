133930a5fb24848f95ac6f5bc179282cf72ea35f
allanvinicius <allanviniciosdemoura@gmail.com>
2024-04-03 17:46:27 -0300

    feat: add pagination in table

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -6,6 +6,9 @@ import {
   ChevronRight,
   ChevronsRight,
 } from "lucide-react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/pt-br";
 import { IconButton } from "./icon-button";
 import { Table } from "./table/table";
 import { TableHeader } from "./table/table-header";
@@ -14,11 +17,33 @@ import { TableRow } from "./table/table-row";
 import { ChangeEvent, useState } from "react";
 import { attendees } from "../data/attendees";
 
+dayjs.extend(relativeTime);
+dayjs.locale("pt-br");
+
 export function AttendeeList() {
   const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+
+  const totalPages = Math.ceil(attendees.length / 10);
 
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value);
+  }
+
+  function goToNextPage() {
+    setPage(page + 1);
+  }
+
+  function goToPreviousPage() {
+    setPage(page - 1);
+  }
+
+  function goToLastPage() {
+    setPage(totalPages);
+  }
+
+  function goToFirstPage() {
+    setPage(1);
   }
 
   return (
@@ -60,59 +85,65 @@ export function AttendeeList() {
         </thead>
 
         <tbody>
-          {attendees.map(({ id, name, email, createdAt, checkedInAt }) => (
-            <TableRow key={id}>
-              <TableCell>
-                <input
-                  type="checkbox"
-                  className="size-4 bg-black/20 rounded border border-white/10 checked:bg-orange-400"
-                />
-              </TableCell>
+          {attendees
+            .slice((page - 1) * 10, page * 10)
+            .map(({ id, name, email, createdAt, checkedInAt }) => (
+              <TableRow key={id}>
+                <TableCell>
+                  <input
+                    type="checkbox"
+                    className="size-4 bg-black/20 rounded border border-white/10 checked:bg-orange-400"
+                  />
+                </TableCell>
 
-              <td>{id}</td>
+                <td>{id}</td>
 
-              <TableCell>
-                <div className="flex flex-col gap-1">
-                  <span className="font-semibold text-white">{name}</span>
-                  <span>{email}</span>
-                </div>
-              </TableCell>
+                <TableCell>
+                  <div className="flex flex-col gap-1">
+                    <span className="font-semibold text-white">{name}</span>
+                    <span>{email}</span>
+                  </div>
+                </TableCell>
 
-              <TableCell>{createdAt.toISOString()}</TableCell>
+                <TableCell>{dayjs().to(createdAt)}</TableCell>
 
-              <TableCell>{checkedInAt.toISOString()}</TableCell>
+                <TableCell>{dayjs().to(checkedInAt)}</TableCell>
 
-              <TableCell>
-                <IconButton transparent>
-                  <MoreHorizontal className="size-4" />
-                </IconButton>
-              </TableCell>
-            </TableRow>
-          ))}
+                <TableCell>
+                  <IconButton transparent>
+                    <MoreHorizontal className="size-4" />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
         </tbody>
 
         <tfoot>
           <tr>
-            <TableCell colSpan={3}>Mostrando 10 de 228 itens</TableCell>
+            <TableCell colSpan={3}>
+              Mostrando 10 de {attendees.length} itens
+            </TableCell>
 
             <TableCell className="text-right" colSpan={3}>
               <div className="inline-flex items-center gap-8">
-                <span>Página 1 de 23</span>
+                <span>
+                  Página {page} de {totalPages}
+                </span>
 
                 <div className="flex gap-1.5">
-                  <IconButton>
+                  <IconButton onClick={goToFirstPage} disabled={page === 1}>
                     <ChevronsLeft className="size-4" />
                   </IconButton>
 
-                  <IconButton>
+                  <IconButton onClick={goToPreviousPage} disabled={page === 1}>
                     <ChevronLeft className="size-4" />
                   </IconButton>
 
-                  <IconButton>
+                  <IconButton onClick={goToNextPage} disabled={page === totalPages}>
                     <ChevronRight className="size-4" />
                   </IconButton>
 
-                  <IconButton>
+                  <IconButton onClick={goToLastPage} disabled={page === totalPages}>
                     <ChevronsRight className="size-4" />
                   </IconButton>
                 </div>
